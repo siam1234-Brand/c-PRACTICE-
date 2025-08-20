0d3bb5a52ac38695e9eec174ef5d67a8b3f75433
SIAM <151781585+siam1234-Brand@users.noreply.github.com>
2025-08-20 22:02:06 +0600

    Add files via upload

---
+++ Bank_Account_Management_System/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Management_System
+{
+    public class BankAccount
+    {
+        protected int accountNumber;
+        protected string holderName;
+        protected double balance;
+        protected static int totalAccounts;
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+
+        public string HolderName
+        {
+            get { return this.holderName; }
+            set { this.holderName = value; }
+        }
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        public static int TotalAccounts
+        {
+            get { return totalAccounts; }
+            set { totalAccounts = value; }
+        }
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(int accountNumber, string holderName, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.holderName = holderName;
+            this.balance = balance;
+        }
+
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine("Account Number: " + accountNumber);
+            Console.WriteLine("Holder Name: " + holderName);
+            Console.WriteLine("Banlance: " + balance);
+        }
+    }
+}
---
+++ Bank_Account_Management_System/CorrentAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Management_System
+{
+    public class CorrentAccount : BankAccount
+    {
+        private double overdraftLimit;
+        private double transactionFee;
+
+        public double OverdraftLimit
+        {
+            get { return this.overdraftLimit; }
+            set { this.overdraftLimit = value; }
+        }
+
+        public double TransactionFee
+        {
+            get { return this.transactionFee; }
+            set { this.transactionFee = value; }
+        }
+
+        public CorrentAccount () : base()
+        {
+
+        }
+
+        public CorrentAccount(int accountNumber, string holderName, double balance, double overdraftLimit, double transactionFee)
+            : base(accountNumber, holderName, balance)
+        {
+            this.overdraftLimit = overdraftLimit;
+            this.transactionFee = transactionFee;
+        }
+
+        public override void ShowDetails()
+        {
+            base.ShowDetails();
+            Console.WriteLine("Overdraft Limit: "+overdraftLimit);
+            Console.WriteLine("Transaction Fee: "+transactionFee);
+        }
+
+        public bool CanWithdraw(double amount)
+        {
+            return amount <= (Balance + overdraftLimit);
+        }
+
+        public bool CanWithdraw(double amount, double fee)
+        {
+            return amount <= (Balance - fee + overdraftLimit);
+        }
+    }
+}
